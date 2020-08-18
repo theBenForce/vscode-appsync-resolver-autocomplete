f26d02df453d60f1fa192e9b6d0e68e1facd9d0b
Ben Force <benforce@gmail.com>
2020-08-18 12:28:10 -0500

    feat: velocity preview

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,18 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 import registerProviders from "./providers";
+import { VelocityPreviewProvider } from "./previewContentProvider";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.info(`appsync resolver extension activated!`);
 
-	console.info(`appsync resolver extension activated!`);
-
-	registerProviders(context);
-
+  registerProviders(context);
+  VelocityPreviewProvider.register(context);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
